bc6ce35f381c7a3871bf7c667237ad2be971449f
Nicolas Erazo <nico.lacho@outlook.com>
2023-01-07 15:42:11 -0500

    Update Profile and Footer!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'FinalChallengeFront';
-  IsAuthenticated = false;
+
 }
